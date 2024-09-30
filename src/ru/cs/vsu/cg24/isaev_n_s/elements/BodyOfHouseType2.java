3ce106cef75b51de12a4e8f801f8e1039ce32a94
Nikolay <nikolayisaev258@gmail.com>
2024-09-30 20:05:38 +0300

    Добавил классы разних типов домов

---
+++ src/ru/cs/vsu/cg24/isaev_n_s/elements/BodyOfHouseType2.java
@@ -0,0 +1,31 @@
+package ru.cs.vsu.cg24.isaev_n_s.elements;
+
+import java.awt.*;
+
+public class BodyOfHouseType2 extends BodyOfHouseType1{
+    public BodyOfHouseType2(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void drawBody(Graphics2D g) {
+        setWidthOfWindow((getX() + getWidth() - 10) - (getX() + 10));
+        setHeightOfWindow(getHeight() / 4);
+        setxOfWindow(getX() + getX() + 10);
+        setyOfWindow(getY() + (getHeight() - getHEIGHT_WINDOW_OVER_FLOOR() - getHeightOfWindow()));
+
+        g.setColor(getColorOfHouse());
+        g.fillRect(getX(), getY(), getWidth(), getHeight()); // рисование дома
+
+        g.setColor(getColorOfWindow());
+        g.fillRect(getxOfWindow(), getyOfWindow(), getWidthOfWindow(), getHeightOfWindow()); // рисование окна
+        g.setColor(Color.BLACK);
+        g.drawLine(getxOfWindow() + getWidthOfWindow() / 2, getyOfWindow(),
+                getxOfWindow() + getWidthOfWindow() / 2, getyOfWindow() + getHeightOfWindow());
+
+       /* g.drawLine(getxOfWindow(), getyOfWindow() + getHeightOfWindow() / 2,
+                getxOfWindow() + getWidthOfWindow(), getyOfWindow() + getHeightOfWindow() / 2);
+
+        */
+    }
+}
---
+++ src/ru/cs/vsu/cg24/isaev_n_s/elements/BodyOfHouseType3WithDoor.java
@@ -0,0 +1,93 @@
+package ru.cs.vsu.cg24.isaev_n_s.elements;
+
+import java.awt.*;
+
+public class BodyOfHouseType3WithDoor {
+    private int x, y, width, height;
+    private final Color colorOfHouse = Color.DARK_GRAY;
+    private Color colorOfDoor;
+    private int xPointOfDoor;
+    private int yPointOfDoor;
+
+    public BodyOfHouseType3WithDoor(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Color getColorOfHouse() {
+        return colorOfHouse;
+    }
+
+    public Color getColorOfDoor() {
+        return colorOfDoor;
+    }
+
+    public void setColorOfDoor(Color colorOfDoor) {
+        this.colorOfDoor = colorOfDoor;
+    }
+
+    public int getxPointOfDoor() {
+        return xPointOfDoor;
+    }
+
+    public void setxPointOfDoor(int xPointOfDoor) {
+        this.xPointOfDoor = xPointOfDoor;
+    }
+
+    public int getyPointOfDoor() {
+        return yPointOfDoor;
+    }
+
+    public void setyPointOfDoor(int yPointOfDoor) {
+        this.yPointOfDoor = yPointOfDoor;
+    }
+
+    public void drawBody(Graphics2D g){
+        // рисование дома
+         g.setColor(getColorOfHouse());
+         g.fillRect(getX(), getY(), getWidth(), getHeight());
+
+        //рисование двери
+         setxPointOfDoor(getX() + getWidth() / 4);
+         setyPointOfDoor(getY() + getHeight() / 3);
+         g.setColor(getColorOfDoor());
+         g.fillRect(getxPointOfDoor(), getyPointOfDoor(), getWidth() / 2, getHeight() / 3 * 2);
+
+        //рисование дверной ручки
+
+     }
+}
